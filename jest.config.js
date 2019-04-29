8a241eab09ebcadd2f8650b43957bd81aa5a15a9
MikeSquall <michael.arbib@om2.com>
2019-04-29 14:40:26 +0300

    Added dependencies setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  setupFiles: [],
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  moduleDirectories: ['src', 'node_modules'],
+  testEnvironment: 'node',
+};
