cbb2aee5b6828901c163859be8afa4c98e8196dd
MikeSquall <michael.arbib@om2.com>
2019-05-02 11:14:02 +0300

    Changes following review

--- src/api/Selector.ts
+++ src/api/Selector.ts
@@ -1,18 +1,20 @@
 import { Observable } from 'rxjs';
 
 /**
- * Selector service provides a solution to temporarily store data and select a part of it.
+ * Selector service provides a solution to input data, get it, select a part of it and get this selected part.
+ * It takes two typed args: Key and Item.
+ * Item is the type of the used object and Key is a subset of Item used to select a specific Item.
  */
 export interface Selector<Item extends Key, Key> {
   /**
-   * @typeparam SetItemsStreamRequest
+   * @typeparam SetItemsRequest
    *
    * The provided stream must be an array of items to be able to perform the selection later.
    *
    * @return A Promise that resolves without specific information.
    * Can be rejected in case of invalid argument is passed.
    */
-  setItemsStream(setItemsStreamRequest: SetItemsStreamRequest<Item>): Promise<void>;
+  setItems(setItemsRequest: SetItemsRequest<Item>): Promise<void>;
   /**
    * @typeparam ItemRequest
    * @return An Observable sequence that describes updates of the stored items.
@@ -20,12 +22,12 @@ export interface Selector<Item extends Key, Key> {
   items$(itemsRequest: ItemsRequest): Observable<Item[]>;
   /**
    * @typeparam SelectItemRequest
-   * @return A Promise that resolves with the selected item.
+   * @return A Promise that resolves without specific information.
    * Can be rejected in case of invalid argument is passed.
    * Can be rejected in case of the item you try to select is not in the stored items.
    * Can be rejected in case of the provided key is not included in the item model.
    */
-  selectItem(selectItemRequest: SelectItemRequest<Key>): Promise<Item>;
+  selectItem(selectItemRequest: SelectItemRequest<Key>): Promise<void>;
   /**
    * @typeparam SelectedItemRequest
    * @return An Observable sequence that describes updates of the selected item.
@@ -33,7 +35,7 @@ export interface Selector<Item extends Key, Key> {
   selectedItem$(selectedItemRequest: SelectedItemRequest): Observable<Item>;
 }
 
-export interface SetItemsStreamRequest<Item> {
+export interface SetItemsRequest<Item> {
   items$: Observable<Item[]>;
 }
 
