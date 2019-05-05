99b094997269857f675f9ba5572703dc6a99e64f
MikeSquall <michael.arbib@om2.com>
2019-05-05 15:59:56 +0300

    Implemented setItems()

--- src/Selector.ts
+++ src/Selector.ts
@@ -1,5 +1,7 @@
 import { ItemsRequest, SelectedItemRequest, SelectItemRequest, SelectorInterface, SetItemsRequest } from './api';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { validationMessages } from './helpers/messages';
+import { isValidSetItemsRequest } from './helpers/validators';
 
 export class Selector<Item extends Key, Key> implements SelectorInterface<Item, Key> {
   private readonly data$: BehaviorSubject<Item[]>;
@@ -11,7 +13,13 @@ export class Selector<Item extends Key, Key> implements SelectorInterface<Item, 
   }
 
   public setItems(setItemsRequest: SetItemsRequest<Item>): Promise<void> {
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+      if (!isValidSetItemsRequest(setItemsRequest)) {
+        reject(new Error(validationMessages.invalidSetItemsRequest));
+      }
+      this.data$.next(setItemsRequest.items);
+      resolve();
+    });
   }
 
   public items$(itemsRequest: ItemsRequest): Observable<Item[]> {
