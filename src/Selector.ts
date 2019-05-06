8da144a73e0ec1f2cf49a63982f240c11af7845b
MikeSquall <michael.arbib@om2.com>
2019-05-06 14:31:08 +0300

    Implemented selection method

--- src/Selector.ts
+++ src/Selector.ts
@@ -1,7 +1,8 @@
 import { ItemsRequest, SelectedItemRequest, SelectItemRequest, SelectorInterface, SetItemsRequest } from './api';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { validationMessages } from './helpers/messages';
-import { isValidSetItemsRequest } from './helpers/validators';
+import { validationMessages, errorMessages } from './helpers/messages';
+import { isValidSelectRequest, isValidSetItemsRequest } from './helpers/validators';
+import { map, take } from 'rxjs/operators';
 
 export class Selector<Item extends Key, Key> implements SelectorInterface<Item, Key> {
   private readonly data$: BehaviorSubject<Item[]>;
@@ -15,10 +16,10 @@ export class Selector<Item extends Key, Key> implements SelectorInterface<Item, 
   public setItems(setItemsRequest: SetItemsRequest<Item>): Promise<void> {
     return new Promise((resolve, reject) => {
       if (!isValidSetItemsRequest(setItemsRequest)) {
-        reject(new Error(validationMessages.invalidSetItemsRequest));
+        return reject(new Error(validationMessages.invalidSetItemsRequest));
       }
       this.data$.next(setItemsRequest.items);
-      resolve();
+      return resolve();
     });
   }
 
@@ -27,7 +28,56 @@ export class Selector<Item extends Key, Key> implements SelectorInterface<Item, 
   }
 
   public selectItem(selectItemRequest: SelectItemRequest<Key>): Promise<void> {
-    return new Promise((resolve, reject) => {});
+    return new Promise((resolve, reject) => {
+      // Reject case: no data has been set in Selector
+      if (this.data$.getValue().length === 0) {
+        return reject(new Error(errorMessages.noData));
+      }
+      // Reject case: selectItemRequest is not valid
+      if (!isValidSelectRequest(selectItemRequest)) {
+        return reject(new Error(validationMessages.invalidSelectItemRequest));
+      }
+
+      const requestKeys = Object.keys(selectItemRequest.key);
+      // Reject case: Key is not a subset of Item
+      if (
+        requestKeys.every((requestKey: any) => {
+          return (
+            Object.keys(this.selected$.getValue()).includes(requestKey) &&
+            // @ts-ignore
+            this.selected$.getValue()[requestKey] === selectItemRequest.key[requestKey]
+          );
+        })
+      ) {
+        return reject(new Error(errorMessages.itemAlreadySelected));
+      }
+
+      this.data$
+        .pipe(
+          take(1),
+          map((items) =>
+            items.find((item, index) => {
+              const itemKeys = Object.keys(item);
+              return requestKeys.every((requestKey) => {
+                return itemKeys.some(
+                  (itemKey) =>
+                    itemKey.includes(requestKey) &&
+                    // @ts-ignore
+                    items[index][itemKey] === selectItemRequest.key[requestKey]
+                );
+              });
+            })
+          )
+        )
+        .subscribe((item: any) => {
+          if (!item) {
+            // Rejection case: wanted Item not
+            return reject(new Error(errorMessages.itemNotFound));
+          }
+          this.selected$.next(item);
+          return resolve();
+        });
+    });
   }
 
   public selectedItem$(selectedItemRequest: SelectedItemRequest): Observable<Item> {
