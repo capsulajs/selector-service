f4b71f084651b74ca2ad333f5337dc7c5f4c6854
MikeSquall <michael.arbib@om2.com>
2019-05-06 17:40:05 +0300

    Added real error/validation messages

--- src/helpers/messages.ts
+++ src/helpers/messages.ts
@@ -1,11 +1,10 @@
 export const errorMessages = {
-  itemNotFound: '',
-  keyNotInItem: '',
-  itemAlreadySelected: '',
-  noData: '',
+  itemNotFound: 'The item you are looking for was not found',
+  itemAlreadySelected: 'The item is already selected',
+  noData: 'No data has been set',
 };
 
 export const validationMessages = {
-  invalidSetItemsRequest: '',
-  invalidSelectItemRequest: '',
+  invalidSetItemsRequest: 'SetItemsRequest must match this pattern: { items: Item[] }',
+  invalidSelectItemRequest: 'SelectItemRequest must match this pattern: { key: Key }',
 };
